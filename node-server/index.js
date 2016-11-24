a5a14ab96358b94025c0ffde5eed3d4a04543926
Assaf Lavie <me@assaflavie.com>
2016-11-24 13:29:51 +0200

    Capture node.js logs using FileBeat

--- node-server/index.js
+++ node-server/index.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var app = express();
 var expressWs = require('express-ws')(app);
+var bunyan = require('bunyan');
+var log = bunyan.createLogger({
+    name: 'chat',
+    streams: [{
+        type: 'rotating-file',
+        path: '/var/log/chat/chat.log',
+        period: '1d',   // daily rotation
+        count: 3        // keep 3 back copies
+    }]
+});
  
 app.get('/api', function(req, res, next){
     res.send("Welcome to express-ws chat API!");
@@ -11,7 +21,7 @@ var rooms = {};
 app.ws('/room/:room', function(ws, req) {
     var room_name = req.params.room;
     if (!(room_name in rooms)) {
-        console.log("New room being opened", room_name);
+        log.info({ name: room_name }, "new room created");
         rooms[room_name] = new Set([ws]);
     } else {
         rooms[room_name].add(ws);
@@ -30,8 +40,9 @@ app.ws('/room/:room', function(ws, req) {
                 throw new Error();
             }
         } catch (error) {
-            console.log("Plain text message treated as from anonymous", msg);
+            log.info({ msg: msg }, "Plain text message (not JSON) treated as from anonymous");
         }
+        log.info({ user: user, message: msg}, "user message");
         var connections = rooms[room_name];
         connections.forEach(function(w) {
             if (w) {
@@ -50,3 +61,4 @@ app.ws('/room/:room', function(ws, req) {
 var port = 3000;
 app.listen(port);
 console.log("Chat listening on port", port);
+log.info({ port: port }, "Chat server is up");
