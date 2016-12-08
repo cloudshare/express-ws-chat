4a97e38c658a3175260dd02d5264705833fb9d01
Assaf Lavie <me@assaflavie.com>
2016-12-08 17:09:31 +0200

    REST API endpoint for fetching room history.

--- node-server/index.js
+++ node-server/index.js
@@ -4,19 +4,64 @@ var express = require('express');
 var app = express();
 var expressWs = require('express-ws')(app);
 var bunyan = require('bunyan');
+var elasticsearch = require('elasticsearch');
+
+
+var log_streams = [{
+    type: 'rotating-file',
+    path: '/var/log/chat/chat.log',
+    period: '1d',   // daily rotation
+    count: 3        // keep 3 back copies
+}];
+
+if (process.env.DEBUG) {
+    log_streams.push({
+        name: 'node',
+        stream: process.stdout,
+        level: 'debug'
+    });
+}
 var log = bunyan.createLogger({
     name: 'chat',
-    streams: [{
-        type: 'rotating-file',
-        path: '/var/log/chat/chat.log',
-        period: '1d',   // daily rotation
-        count: 3        // keep 3 back copies
-    }]
+    streams: log_streams
 });
- 
-app.get('/api', function(req, res, next){
-    res.send("Welcome to express-ws chat API!");
-    next();
+
+var es_host = process.env.ES_HOST ? process.env.ES_HOST : "es";
+var es_url = es_host + ":9200";
+
+var es_client = new elasticsearch.Client({ host: es_url });
+
+function fetch_room_history(room, done) {
+    log.debug({ room: room }, "fetching room history from elasticsearch");
+    es_client.search({
+        "type": "chat_message",
+        "body" : {
+            "query" : { "term" : { "room": "room_" + room } },
+            "sort": [{ "sent": "desc" }],
+            "from": 0,
+            "size": 10
+        }
+    }).then(function(response) {
+        if (done) {
+            done(response.hits);
+        }
+    }, function(error) {
+        log.error(error);
+    });
+}
+
+app.get('/api/history/:room', function(req, res){
+    var room = req.params.room;
+    fetch_room_history(room, function(hits) {
+        res.setHeader('Content-Type', 'application/json');
+        var history = hits.hits.map(function(hit) {
+            var s = hit._source;
+            return { msg: s.message, time: s.sent, user: s.user };
+        });
+        var body = JSON.stringify({ messages: history});
+        res.write(body);
+        res.end();
+    });
 });
 
 var rooms = {};
@@ -62,13 +107,16 @@ app.ws('/room/:room', function(ws, req) {
         } catch (error) {
             log.info({ msg: msg }, "Plain text message (not JSON) treated as from anonymous");
         }
-        log.info({ user: user, message: msg, room: room_name}, "user message");
         if (msg == "_announce_") {
             send(ws, "Room", "Welcome to chat room " + room_name);
             msg = user + " has joined the chat";
             user = "Room";
         }
         broadcast(room, ws, user, msg);
+        if (msg != "_announce") {
+            log.info({ user: user, message: msg, room: "room_" + room_name, type: "chat_message",
+                sent: new Date()}, "user message");
+        }
     });
 
     send(ws, "Room", "_whois_");
